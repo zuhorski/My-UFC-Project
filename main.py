0d32fb5985b14e3a0d297fc7e398f283bb85e041
sabzuski <sabzuski@gmail.com>
2023-02-08 12:15:34 -0500

    experimentation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import psycopg2
 import streamlit as st
 import psycopg2
+import pandas as pd
+
+st.title("UFC Project")
 
 
 @st.experimental_singleton
@@ -10,14 +13,54 @@ def init_connection():
 
 conn = init_connection()
 
+
 @st.experimental_memo(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
-st.write(run_query('Select * from events;'))
 
-st.title("UFC Project")
+
+df = pd.DataFrame(run_query('Select * from fightstats;'),
+                  columns=[
+                    'Event',
+                    'Bout',
+                    'Weightclass',
+                    'TitleFight',
+                    'Round',
+                    'Fighter',
+                    'KD',
+                    'SIG_STR_LAND',
+                    'SIG_STR_ATT',
+                    'TOTAL_STR_LAND',
+                    'TOTAL_STR_ATT',
+                    'TD',
+                    'TD_ATT',
+                    'SUB_ATT',
+                    'REVERSAL',
+                    'CTRL_TIME_SEC',
+                    'HEAD_LAND',
+                    'HEAD_ATT',
+                    'BODY_LAND',
+                    'BODY_ATT',
+                    'LEG_LAND',
+                    'LEG_ATT',
+                    'STAND_STR_LAND',
+                    'STAND_STR_ATT',
+                    'CLINCH_STR_LAND',
+                    'CLINCH_STR_ATT',
+                    'GROUND_STR_LAND',
+                    'GROUND_STR_ATT',
+                    'WINNER',
+                    'WIN_BY',
+                    'LAST_ROUND',
+                    'ROUND_TIME_SEC',
+                    'FORMAT',
+                    'URL'])
+
+st.dataframe(df)
+
+
 # login, signup = st.tabs(['Login', 'Signup'])
 # with login:
 #     with st.form(key='LoginForm'):
@@ -33,8 +76,5 @@ st.title("UFC Project")
 #     pword = c.text_input('Password')
 #     dataTuple = (uname, pword)
 
-
-
-
-if dataTuple[0] != '' and dataTuple[1] != '':
-    st.write(dataTuple)
+# if dataTuple[0] != '' and dataTuple[1] != '':
+#     st.write(dataTuple)
