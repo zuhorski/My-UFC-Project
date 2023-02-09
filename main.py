dfdd0a2037d06e2744aa65c8bbfee78930cd0047
sabzuski <sabzuski@gmail.com>
2023-02-09 10:30:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -58,22 +58,3 @@ df = pd.DataFrame(run_query('Select * from fightstats;'),
                     'URL'])
 
 st.dataframe(df)
-
-
-# login, signup = st.tabs(['Login', 'Signup'])
-# with login:
-#     with st.form(key='LoginForm'):
-#         uname1 = st.text_input('Username', key='User')
-#         pword1 = st.text_input('Password', key='Password')
-#         dataTuple = (uname1, pword1)
-#         submitted = st.form_submit_button('Submit')
-#
-#
-# with signup:
-#     c = st.container()
-#     uname = c.text_input('Username')
-#     pword = c.text_input('Password')
-#     dataTuple = (uname, pword)
-
-# if dataTuple[0] != '' and dataTuple[1] != '':
-#     st.write(dataTuple)
